0cf6c0d55c6ac2c77667ecfec0a517ee325f773c
Romashka093 <aa.romashchenko@gmail.com>
2019-11-12 17:39:57 +0200

    JS hw-03 coomit-02

--- javaScript/goit-js-hw-03/HW-module-03.js
+++ javaScript/goit-js-hw-03/HW-module-03.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// console.log("TASK-01");
 // const user = {
 //   name: "Mango",
 //   age: 20,
@@ -7,46 +7,12 @@
 //   premium: true
 // };
 
-// // 1. добавляет поле mood со значением 'happy'
 // user.mood = "happy";
-// console.log(user.mood);
-
-// // 2. заменяет значение hobby на 'javascript'
 // user.hobby = "javascript";
-// console.log(user.hobby);
-
-// // 3. заменяет значение premium на false
 // user.premium = false;
-// console.log(user.premium);
-// // console.log(
-// //   "#4 - выводит содержимое объекта user в формате ключ:значение используя Object.keys() и for...of"
-// // );
-
-// // console.log("option (need to comment out the previous code in title of task)");
-
-// // const user = (name, age, hobby, premium, mood) => {
-// //   return {
-// //     name,
-// //     age,
-// //     hobby,
-// //     premium,
-// //     mood
-// //   };
-// // };
-// // console.log(user("Anna", 30, "JS", "true", "OK"));
-
-// console.log("first option");
-// const contentOfObgect = Object.entries(user);
-// for (const content of contentOfObgect) {
-//   const key = content[0];
-//   const value = content[1];
-//   console.log(`${key}: ${value}`);
-// }
-
-// console.log("second option");
-// const contentOfValue = Object.keys(user);
-// for (const content of contentOfValue) {
-//   console.log("value: ", user[content]);
+// const keys = Object.keys(user);
+// for (const key of keys) {
+//   console.log(`${key}:`, user[key]);
 // }
 
 // console.log("TASK-02");
@@ -56,13 +22,15 @@
 //   return key.length;
 // };
 
+// // const countProps = obj => Object.keys(obj).length;
+
 // console.log(countProps({})); // 0
 // console.log(countProps({ name: "Mango", age: 2 })); // 2
 // console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
 
 // console.log("TASK-03");
 
-// const findBestEmployee = function(employees) {
+// function findBestEmployee(employees) {
 //   let max = 0;
 //   let name;
 
@@ -75,12 +43,10 @@
 //     }
 //   }
 //   return name;
-// };
+// }
 
 // console.log(findBestEmployee({ ann: 29, david: 35, helen: 1, lorence: 99 }));
-
 // console.log(findBestEmployee({ poly: 12, mango: 17, ajax: 4 }));
-
 // console.log(findBestEmployee({ lux: 147, david: 21, kiwi: 19, chelsy: 38 }));
 
 // console.log("TASK-04");
@@ -94,22 +60,8 @@
 // };
 
 // console.log(countTotalSalary({})); // 0
-
-// console.log(
-//   countTotalSalary({
-//     mango: 100,
-//     poly: 150,
-//     alfred: 80
-//   })
-// ); // 330
-
-// console.log(
-//   countTotalSalary({
-//     kiwi: 200,
-//     lux: 50,
-//     chelsy: 150
-//   })
-// ); // 400
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 })); // 330
+// console.log(countTotalSalary({ kiwi: 200, lux: 50, chelsy: 150 })); // 400
 
 // console.log("TASK-05");
 
@@ -134,25 +86,25 @@
 // console.log(getAllPropValues(products, "quantity"));
 // console.log(getAllPropValues(products, "category"));
 
-console.log("TASK-06");
+// console.log("TASK-06");
 
-const products = [
-  { name: "Радар", price: 1300, quantity: 4 },
-  { name: "Сканер", price: 2700, quantity: 3 },
-  { name: "Дроид", price: 400, quantity: 7 },
-  { name: "Захват", price: 1200, quantity: 2 }
-];
+// const products = [
+//   { name: "Радар", price: 1300, quantity: 4 },
+//   { name: "Сканер", price: 2700, quantity: 3 },
+//   { name: "Дроид", price: 400, quantity: 7 },
+//   { name: "Захват", price: 1200, quantity: 2 }
+// ];
 
-const calculateTotalPrice = function(products, productName) {
-  let total = 0;
+// const calculateTotalPrice = function(products, productName) {
+//   let total = 0;
 
-  for (let product of products) {
-    if (productName === product.name) {
-      total = product.price * product.quantity;
-      return total;
-    }
-  }
-};
+//   for (let product of products) {
+//     if (productName === product.name) {
+//       total = product.price * product.quantity;
+//       return total;
+//     }
+//   }
+// };
 
-console.log(calculateTotalPrice(products, "Радар")); // 5200
-console.log(calculateTotalPrice(products, "Дроид")); // 2800
+// console.log(calculateTotalPrice(products, "Радар")); // 5200
+// console.log(calculateTotalPrice(products, "Дроид")); // 2800
