25bd05a499eb2365e1bfff9ffc33e02cd777e93a
Romashka093 <aa.romashchenko@gmail.com>
2019-12-18 12:37:51 +0200

    Updates

---
+++ precache-manifest.0f8a61d59638a2ce08b34f927e4b44f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9feb9194dd6fec21fe372e7188002e53",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "157d39a1696c38eca426",
+    "url": "/goit-react-hw-01-components/static/css/main.c934ded4.chunk.css"
+  },
+  {
+    "revision": "caceef7bab207ba9dce6",
+    "url": "/goit-react-hw-01-components/static/js/2.ef1cf6e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-01-components/static/js/2.ef1cf6e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "157d39a1696c38eca426",
+    "url": "/goit-react-hw-01-components/static/js/main.fcb85cf2.chunk.js"
+  },
+  {
+    "revision": "351ec24c9bd9afb33e31",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.b5c70b18.js"
+  }
+]);
