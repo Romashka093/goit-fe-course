e9c61e9a7719d4a43236078f8d88ef6145bbd025
Romashka093 <aa.romashchenko@gmail.com>
2019-12-18 12:22:05 +0200

    added default page

--- React/goit-react-hw-01-components/src/App.js
+++ React/goit-react-hw-01-components/src/App.js
@@ -3,7 +3,7 @@ import Profile from './components/Profile/Profile';
 import user from './user.json';
 import Statistics from './components/Statistics/Statistics';
 import statisticalData from './statistical-data.json';
-
+console.log('he is a life!');
 const App = () => (
   <>
     <Profile user={user} />
